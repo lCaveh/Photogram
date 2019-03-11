9b2f2b854950ec9163e10cb9c537fc8972426a08
ks <lcaveh@gmail.com>
2019-03-11 12:12:35 -0700

    Footer fixed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,23 +5,21 @@ import UIkit from "uikit";
 function Footer(props) {
 
     return (
-        <div className="footer">
-            <nav className="uk-navbar-container uk-margin" data-uk-navbar>
-            <div className="uk-navbar-center">
-
-                <div className="uk-navbar-left">
-                    <NavLink to="/profile" className="uk-navbar-toggle">Profile</NavLink>
-                </div>
+        <div >
+            <nav className="uk-navbar-container uk-position-bottom uk-position-fixed footer" data-uk-navbar>
                 <div className="uk-navbar-center">
-                    <NavLink to="/privateposts" className="uk-navbar-toggle">Private</NavLink>
-                </div>
-                <div className="uk-navbar-right uk-active">
-                    <NavLink to="/" className="uk-navbar-toggle">All Posts</NavLink>
-                </div>
-            </div>
 
+                    <div className="uk-navbar">
+                        <NavLink to="/profile" className="uk-navbar-toggle">Profile</NavLink>
+                    </div>
+                    <div className="uk-navbar ">
+                        <NavLink to="/privateposts" className="uk-navbar-toggle">Private</NavLink>
+                    </div>
+                    <div className="uk-navbar uk-active">
+                        <NavLink to="/" className="uk-navbar-toggle">All Posts</NavLink>
+                    </div>
+                </div>
             </nav>
-
         </div>
     )
 }
